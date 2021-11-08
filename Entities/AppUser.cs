cd0c11083e51f994016879fe15f69e778d3de805
kirschadam <78801902+kirschadam@users.noreply.github.com>
2021-11-08 12:54:20 +0100

    Add files via upload

---
+++ Entities/AppUser.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entries
+{
+    public class AppUser
+    {
+        public int ID {get; set; }
+
+        public string Username { get; set; }
+
+        
+    }
+}
